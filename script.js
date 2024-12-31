c5eb0da51b980517fbb1d030d423712dfb0b3e85
Marcos Duarte <pe.marcos30@gmail.com>
2024-12-31 14:02:02 -0300

    Atualizando video e texto

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ playButton.addEventListener('click', () => {
     <div class="popup-content">
       <span class="close-button">&times;</span>
       <div style="position: relative; padding-bottom: 56.25%; height: 0;">
-        <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/ofFwUajya1U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/D1jz2qz_jQY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
   `;
